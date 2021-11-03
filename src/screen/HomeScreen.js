c95f0b56f77fe82a4c3129278579ced77bb6788c
kriptonhaz <kriptonhaz@gmail.com>
2021-11-03 21:24:25 +0700

    implement checkbox and text input

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
+const { width, height } = Dimensions.get("screen");
 
 export function HomeScreen(){
+  const [item, setItem] = React.useState({
+    name: 'Minyak Goreng',
+    check: false
+  })
   return(
     <View style={styles.rootContainer}>
       <Text style={styles.text}>Home Screen</Text>
+      <View style={styles.itemContainer}>
+        <CheckBox 
+          value={item.check}
+          tintColors={{
+            true: '#fc7f03',
+            false: '#fc7f03'
+          }}
+          onValueChange={(newValue) => setItem({...item, check: newValue})}
+        />
+        <TextInput 
+          style={styles.itemText}
+          value={item.name}
+          onChangeText={(val) => setItem({...item, name: val})}
+        />
+      </View>
     </View>
   )
 }
@@ -18,5 +40,14 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'black'
+  },
+  itemContainer: {
+    width: width*0.8,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  itemText: {
+    color: 'black',
+    marginLeft: 5,
   }
 })
